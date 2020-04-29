b592c0b791f5af85ddcc083b6616d11ee72d6bad
Florian Bose <fuselmus-gesischt@web.de>
2020-04-29 12:31:58 +0200

    Logical Names angepasst.

--- Kreditrechner/MainPage.xaml.cs
+++ Kreditrechner/MainPage.xaml.cs
@@ -12,67 +12,71 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-//using Kreditrechner.Daten;
-//using Kreditrechner.Model;
+using Kreditrechner.Daten;
+using Kreditrechner.Model;
 
-// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
-/// TODO
+//Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+/// <TODO>
+/// 01: DataGridView richig verweisen (vom Typ her)
 /// 
+/// </TODO>
+
+
 namespace Kreditrechner
 {
-    /// <summary>
-    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
-        //private InputData Input;
-        //private OutputData Output;
+        private InputData Input;
+        private OutputData Output;
         public MainPage()
         {
             this.InitializeComponent();
-            //    Input = new InputData(this); //Mit "this" referenzieren wir auf die Klasse selbst, also auf das Objekt der Klasse MyForm
-            //    Output = new OutputData(this);
+            Input = new InputData(this); //Mit "this" referenzieren wir auf die Klasse selbst, also auf das Objekt der Klasse MyForm
+            Output = new OutputData(this);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //    // 1. Daten aus Formular auslesen
-            //    Input.getInput();
-            //    // 2. Daten validieren (Wertebereich)
-            //    if (Input.CheckData() == true)
-            //    {
-            //        Annuitaet annuitaet = new Annuitaet(Input);
-            //        annuitaet.Input = Input;
-            //        // 3. Berechnung durchführen
-            //        OutputData[] outputData = annuitaet.BerechneDarlehen();
-            //        // 4. Daten an Formular ausgeben
-            //        Output.setOutput(outputData);
 
-            //    }
-            //}
-            //public string getKreditbetrag()
-            //{
-            //    return textBoxKreditbetrag.Text;
-            //}
-            //public string getZinssatz()
-            //{
-            //    return textBoxZinsatz.Text;
-            //}
-            //public string getTilgungssatz()
-            //{
-            //    return textBoxTilgungssatz.Text;
-            //}
-            //public string getLaufzeit()
-            //{
-            //    return textBoxLaufzeit.Text;
-            //}
-            //public DateTime getDate()
-            //{
-            //    return dateTimePicker.Value;
-            //}
-            //public DataGridView getDGV()
-            //{
-            //    return dataGridView;
+
+            // 1. Daten aus Formular auslesen
+            Input.getInput();
+            // 2. Daten validieren (Wertebereich)
+            if (Input.CheckData() == true)
+            {
+                Annuitaet annuitaet = new Annuitaet(Input);
+                annuitaet.Input = Input;
+                // 3. Berechnung durchführen
+                OutputData[] outputData = annuitaet.BerechneDarlehen();
+                // 4. Daten an Formular ausgeben
+                Output.setOutput(outputData);
+
+            }
+        }
+        public string getKreditbetrag()
+        {
+            return textBoxKreditbetrag.Text;
+        }
+        public string getZinssatz()
+        {
+            return textBoxZinsatz.Text;
+        }
+        public string getTilgungssatz()
+        {
+            return textBoxTilgungssatz.Text;
+        }
+        public double getLaufzeit()
+        {
+            return sliderLaufzeit.Value;
+        }
+        public DateTimeOffset getDate()
+        {
+            return dateTimePicker.Date;
+        }
+        public DataGridView getDGV()
+        {
+            return dataGridView;
         }
     }
 }
